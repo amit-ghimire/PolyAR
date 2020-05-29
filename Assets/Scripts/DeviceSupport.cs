246b9ca16613a97a126b9e82a9a9968c7b363c18
amit07 <amitghimiray07@outlook.com>
2020-05-29 15:47:38 +0545

    Setup AR

---
+++ Assets/Scripts/DeviceSupport.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.SceneManagement;
+using System;
+
+public class DeviceSupport : MonoBehaviour
+{
+    [SerializeField]
+    ARSession m_session;
+
+    private void OnEnable()
+    {
+        ARSession.stateChanged += onSessionStateChange;
+    }
+
+    private void onSessionStateChange(ARSessionStateChangedEventArgs obj)
+    {
+        switch (obj.state)
+        {
+            case ARSessionState.CheckingAvailability:
+                break;
+            case ARSessionState.NeedsInstall:
+                ShowSessionStateMessage();
+                break;
+            case ARSessionState.Installing:
+                ShowSessionStateMessage();
+                break;
+            case ARSessionState.Ready:
+                ShowSessionStateMessage();
+                break;
+            case ARSessionState.SessionInitializing:
+                ShowSessionStateMessage();
+                break;
+            case ARSessionState.SessionTracking:
+                ShowSessionStateMessage();
+                break;
+            case ARSessionState.Unsupported:
+                ShowSessionStateMessage();
+                break;
+        }
+    }
+
+    private void ShowSessionStateMessage() {
+    
+    }
+}
